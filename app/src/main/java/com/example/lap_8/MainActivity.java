9b49c9efaccb64f5949c8f1107ef50c6b6697222
SangVan <vansangdangiuh@gmail.com>
2022-04-18 21:06:03 +0700

    Nop bai

---
+++ app/src/main/java/com/example/lap_8/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.lap_8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.btnSignIn);
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login__page.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register__page.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
